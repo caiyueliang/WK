68f5b3c9829f9b44748e61db7d0c3520066d9c93
yangsheng <690963696@qq.com>
2026-06-05 16:21:04 +0800

    update

--- G1Nav2D/devel/local_setup.fish
+++ G1Nav2D/devel/local_setup.fish
@@ -5,7 +5,7 @@
 # or fall back to the destination set at configure time
 
 if test -z $_CATKIN_SETUP_DIR
-    set _CATKIN_SETUP_DIR /home/water/WK/G1Nav2D/devel
+    set _CATKIN_SETUP_DIR /workspace/WK/G1Nav2D/devel
 end
 
 set CATKIN_SETUP_UTIL_ARGS "--extend --local"
--- G1Nav2D/devel/setup.fish
+++ G1Nav2D/devel/setup.fish
@@ -18,7 +18,7 @@ if not type -q bass
 end
 
 if test -z $_CATKIN_SETUP_DIR
-    set _CATKIN_SETUP_DIR /home/water/WK/G1Nav2D/devel
+    set _CATKIN_SETUP_DIR /workspace/WK/G1Nav2D/devel
 end
 
 set _SETUP_UTIL "$_CATKIN_SETUP_DIR/_setup_util.py"
